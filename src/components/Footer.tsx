2d74e4b2cc96ca04b1804acf53dbac803fed5dc3
Christian Villamin <ChristianVillamin31@gmail.com>
2022-10-22 22:10:17 +0800

    feat: footer, layout adjustment

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+
+const Footer: FC = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="h-12 bg-slate-800 text-slate-200 flex justify-center items-center mt-12">
+      © Christian Villamin {currentYear}. All Rights Reserved.
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
+import Footer from "./Footer";
 
 type Props = {
   children: JSX.Element;
 };
 
 const Layout: FC<Props> = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow max-w-3xl mx-auto">{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
