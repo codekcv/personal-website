6a42e466807aa70eba9ee8f5ffeff480fb89dc21
Christian Villamin <ChristianVillamin31@gmail.com>
2022-10-24 18:43:54 +0800

    feat: navbar desktop responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout: FC<Props> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-grow max-w-3xl mx-auto">{children}</main>
+      <main className="flex-grow max-w-3xl mx-auto md:pt-12">{children}</main>
       <Footer />
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,10 +36,10 @@ const Navbar: FC = () => {
   };
 
   return (
-    <nav className="fixed bottom-0 h-14 bg-white w-screen flex justify-evenly items-center shadow-inner md:top-0">
+    <nav className="fixed bottom-0 z-50 h-14 bg-white w-screen flex justify-evenly items-center shadow-inner md:top-0 md:max-w-3xl md:left-1/2 md:-translate-x-1/2 md:shadow-md">
       <ul className="flex justify-evenly items-center flex-grow">
         {menus.map((menu) => (
-          <li className="w-full pt-2" key={menu.name}>
+          <li className="w-full pt-2 cursor-pointer" key={menu.name}>
             <Link href={menu.link}>
               <div className={`${menuStyle} ${isActiveRoute(menu.link)}`}>
                 <menu.Icon className="text-xl" />
