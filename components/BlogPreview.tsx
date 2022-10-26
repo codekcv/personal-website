d4e4f3ad9c9c564cfcef5aee09dc603445293f21
Christian Villamin <ChristianVillamin31@gmail.com>
2022-10-27 00:09:50 +0800

    ft: rmv global transition, hide desc

--- components/BlogPreview.tsx
+++ components/BlogPreview.tsx
@@ -10,15 +10,15 @@ type Props = {
 const BlogPreview: FC<Props> = ({ title, description, date }) => {
   return (
     <div className="shadow-md p-4 text-left">
-      <h2 className="text-xl font-medium">{title}</h2>
-      <p className="text-xs text-zinc-500 my-2">Posted on {date}</p>
+      <h2 className="text-md font-medium">{title}</h2>
+      <p className="text-xs text-zinc-500 mt-2">Posted on {date}</p>
 
-      <p className="mt-1">
+      {/* <p className="mt-1">
         {description}...{" "}
         <Link href="/">
           <span className="cursor-pointer text-blue-700">Read more.</span>
         </Link>
-      </p>
+      </p> */}
     </div>
   );
 };
