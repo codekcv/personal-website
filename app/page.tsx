28d5f2e07bf50b3bf4727f75d1d736ccc9cba7d8
Christian Villamin <ChristianVillamin31@gmail.com>
2022-10-26 23:30:39 +0800

    gen: use next/font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Amatic_SC } from "@next/font/google";
 import Blogs from "./(home)/Blogs";
 import Hero from "./(home)/Hero";
-import Projects from "./(home)/Project";
+// import Projects from "./(home)/Project";
 import Stack from "./(home)/Stack";
+
+const amatic_sc = Amatic_SC({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
@@ -10,13 +16,12 @@ export default function Home() {
         <Hero />
 
         <div className="px-4">
-          <h1 className="text-center mt-4">
+          <h1 className={`${amatic_sc.className} text-center mt-4`}>
             My personal slice of the internet.
           </h1>
 
           <Blogs />
           <Stack />
-          <Projects />
         </div>
       </section>
 
--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const SectionTitle: FC<Props> = ({ children }) => {
   return (
-    <h1 className="text-center" style={amatic_sc.style}>
+    <h1 className={`${amatic_sc.className} text-center`}>
       &#47;&#47; --------- &lt;
       <span className="text-green-700 font-medium">{children}</span> /&gt;
       --------- &#47;&#47;
