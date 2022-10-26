aa5cafc9ed8df10b1e597ded874df66e20505575
Christian Villamin <ChristianVillamin31@gmail.com>
2022-10-26 13:05:47 +0800

    chore: migrate index route from pages to app

---
+++ app/(home)/Stack.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "@/components/SectionTitle";
+
+export const Stack = () => {
+  return (
+    <section className="mt-8">
+      <SectionTitle>Stack</SectionTitle>
+    </section>
+  );
+};
+
+export default Stack;
