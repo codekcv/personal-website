6a1e1eebafa2d27d395e6e6ba3a7fcd00f96f428
Christian Villamin <ChristianVillamin31@gmail.com>
2022-11-10 20:23:58 +0800

    feat: stacks section, update about

--- app/(home)/About.tsx
+++ app/(home)/About.tsx
@@ -1,5 +1,3 @@
-import Title from "components/Title";
-
 const spanStyles = "indent-4 block font-bold text-lg mb-2";
 
 type ExperienceCardProps = {
@@ -18,8 +16,13 @@ const ExperienceCard = ({
   points,
 }: ExperienceCardProps) => {
   return (
-    <div className="flex mt-8">
-      <p className="text-gray-600 text-sm w-36 pt-[4px]">{date}</p>
+    <div className="mt-8 md:flex">
+      <div className="flex items-center md:items-baseline">
+        <span className="w-2 h-2 ml-[5px] rounded-full bg-slate-500 inline-block md:hidden" />
+        <p className="pl-4 text-gray-600 text-sm w-full pt-0 md:w-40 md:pt-[4px] md:text-sm">
+          {date}
+        </p>
+      </div>
       <div className="flex">
         <div className="border-[1px] ml-2 mr-4"></div>
         <div>
@@ -32,9 +35,10 @@ const ExperienceCard = ({
           <ul>
             {points.map((point, idx) => (
               <li key={point}>
-                <p>
+                <p className="text-sm md:text-base">
                   <span className="text-sm text-gray-600 ml-2 mr-4 my-2 inline-block">
-                    0{idx + 1}
+                    {idx < 9 ? 0 : null}
+                    {idx + 1}
                   </span>
                   {point}
                 </p>
@@ -49,10 +53,8 @@ const ExperienceCard = ({
 
 export default function About() {
   return (
-    <div className="relative h-[calc(100vh-3.5rem)] text-center flex justify-center items-center">
+    <div className="relative text-center flex justify-center items-center">
       <div>
-        <Title className="text-6xl">About</Title>
-
         <div
           className="bg-[#f5f5f5] mt-8 p-4 text-left"
           style={{ boxShadow: "0 0 30px -10px rgba(150, 170, 180, 1)" }}
@@ -61,7 +63,13 @@ export default function About() {
             <span className={spanStyles}>About Me</span>
             <p className="font-mono text-sm">
               Hi, I&apos;m Christian Villamin, a software engineer specializing
-              in JavaScript, React and Node.
+              in JavaScript, React, and Node. I enjoy analyzing codebases,
+              optimizing. Making things efficient, code and work process.
+              Recently, I&apos;ve been also into leadership.
+            </p>
+
+            <p className="font-mono text-sm mt-4">
+              I&apos;ve started programming when I was 10.
             </p>
           </div>
 
@@ -70,23 +78,34 @@ export default function About() {
 
             <ExperienceCard
               date="Aug 2021 - Aug 2022"
-              title="Senior Front-End Engineer"
+              title="Senior Software Engineer"
               company="NextPay (YC W21)"
               href="https://nextpay.world/"
               points={[
+                "Mentoring and code pairing with Front-End Engineers for best practices.",
+                "Communicating with backend engineers and devops for a seamless workflow with the frontend team.",
                 "Writing meaningful and structured documentations.",
-                "Code base health check and clearing tech debts.",
-                "Introduced TypeScript into the codebase for type safety.",
-                "Mentoring Junior/Mid Front-End Engineers for best practices.",
+                "Codebase health check and clearing tech debts.",
+                "Refactoring convoluted code into a more efficient and clean code.",
+                "Writing automated unit and integration tests.",
+                "Setting up analytics and platform monitoring.",
+                "Introduced TypeScript into the codebase for type safety and error catching",
+                "Setting up code sharing sessions to keep everyone in the latest.",
+                "Syncing with engineers for a streamlined and efficient workflow.",
               ]}
             />
 
             <ExperienceCard
               date="Dec 2019 — Jun 2021"
-              title="Junior/Mid Level Front-End Engineer"
+              title="Junior/Mid Level Software Engineer"
               company="High Output Ventures"
               href="https://hov.co/"
-              points={["Performance Optimization"]}
+              points={[
+                "Performance optimization and memory management.",
+                "Interviewing potential frontend candidates.",
+                "Responsible for programming sophisticated animations.",
+                "Initiating ",
+              ]}
             />
           </div>
 
@@ -96,6 +115,8 @@ export default function About() {
               Core Team at https://reactjs.org.ph/
             </p>
             <p>Moderator @ ReactJS Philippines FB Group</p>
+
+            <p>I enjoy playing the piano and guitar.</p>
           </div>
         </div>
       </div>
--- app/(home)/Stack.tsx
+++ app/(home)/Stack.tsx
@@ -1,9 +1,138 @@
-import SectionTitle from "components/SectionTitle";
+import Title from "components/Title";
+
+const frontend = [
+  { title: "React", href: "https://reactjs.org/" },
+  { title: "TypeScript", href: "https://www.typescriptlang.org/" },
+  { title: "Next.js", href: "https://nextjs.org/" },
+  { title: "Apollo Client", href: "https://www.apollographql.com/docs/react/" },
+  { title: "Redux", href: "https://redux.js.org/" },
+  { title: "Tailwind CSS", href: "https://tailwindcss.com/" },
+  { title: "styled-components", href: "https://styled-components.com/" },
+  { title: "Framer Motion", href: "https://www.framer.com/motion/" },
+  { title: "React Hook Form", href: "https://react-hook-form.com/" },
+  { title: "Formik", href: "https://formik.org/" },
+];
+
+const backend = [
+  { title: "TypeScript", href: "https://www.typescriptlang.org/" },
+  { title: "Node.js", href: "https://nodejs.org/en/" },
+  { title: "Express", href: "https://expressjs.com/" },
+  { title: "GraphQL", href: "https://graphql.org/" },
+  { title: "tRPC", href: "https://trpc.io/" },
+  { title: "Supabase", href: "https://supabase.com/" },
+  { title: "NestJS", href: "https://nestjs.com/" },
+  {
+    title: "Apollo Server",
+    href: "https://www.apollographql.com/docs/apollo-server/",
+  },
+  { title: "PostgreSQL", href: "https://www.postgresql.org/" },
+  { title: "MongoDB", href: "https://www.mongodb.com/" },
+  { title: "Socket.IO", href: "https://socket.io/" },
+  { title: "Prisma", href: "https://www.prisma.io/" },
+  { title: "Sanity", href: "https://www.sanity.io/" },
+];
+
+const mobile = [
+  { title: "React Native", href: "https://reactnative.dev/" },
+  { title: "NativeBase", href: "https://nativebase.io/" },
+  { title: "NativeWind", href: "https://nativewind.dev" },
+  { title: "Expo", href: "https://expo.dev/" },
+  {
+    title: "Bottom Sheet",
+    href: "https://gorhom.github.io/react-native-bottom-sheet/",
+  },
+];
+
+const development = [
+  { title: "Git", href: "https://git-scm.com/" },
+  { title: "VS Code", href: "https://code.visualstudio.com/" },
+  { title: "Linux", href: "https://www.linux.org/" },
+  { title: "ESLint", href: "https://www.docker.com/" },
+  { title: "Prettier", href: "https://www.docker.com/" },
+  { title: "Notion", href: "https://www.notion.so/" },
+  { title: "Figma", href: "https://www.figma.com/" },
+  { title: "Storybook", href: "https://storybook.js.org/" },
+  { title: "Datadog", href: "https://www.datadoghq.com/" },
+  { title: "Mixpanel Analytics", href: "https://mixpanel.com/" },
+];
+
+const devops = [
+  {
+    title: "Containers",
+    href: "",
+  },
+  {
+    title: "Shell Scripting",
+    href: "",
+  },
+  { title: "CI/CD", href: "" },
+  { title: "Logging", href: "" },
+  { title: "Cloud", href: "" },
+  { title: "Monitoring", href: "" },
+];
+
+const AWS = [
+  { title: "IAM", href: "https://www.figma.com/" },
+  { title: "EC2", href: "https://storybook.js.org/" },
+  { title: "S3", href: "https://storybook.js.org/" },
+  { title: "Cloud Front", href: "https://storybook.js.org/" },
+  { title: "Lambda", href: "https://storybook.js.org/" },
+  { title: "Load Balancing", href: "https://www.datadoghq.com/" },
+  { title: "Auto Scaling", href: "https://mixpanel.com/" },
+  { title: "EBS", href: "https://www.notion.so/" },
+  { title: "EFS", href: "https://www.notion.so/" },
+  { title: "RDS", href: "https://www.notion.so/" },
+];
+
+const testing = [
+  { title: "Cypress", href: "" },
+  { title: "React Testing Library", href: "" },
+  { title: "Jest", href: "" },
+  { title: "Mocha", href: "" },
+];
+
+type CardProps = {
+  title: string;
+  technologies: Array<{
+    title: string;
+    href: string;
+  }>;
+};
+
+const Card = ({ title, technologies }: CardProps) => {
+  return (
+    <div className="text-center max-w-sm">
+      <h2 className="font-bold text-2xl">{title}</h2>
+      <ul className="flex flex-wrap justify-evenly gap-x-2 gap-y-4 mt-4">
+        {technologies.map((tech: { title: string; href: string }) => (
+          <li key={tech.title}>
+            <a
+              className="border-blue-400 border-[1px] rounded-lg px-2 py-1"
+              href={tech.href}
+            >
+              {tech.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export const Stack = () => {
   return (
-    <section className="mt-8">
-      <SectionTitle>Stack</SectionTitle>
+    <section className="mt-32">
+      <Title className="text-6xl">Skills</Title>
+
+      <div className="flex flex-wrap justify-evenly gap-y-8 mt-8">
+        <Card title="Front-End" technologies={frontend} />
+        <Card title="Back-End" technologies={backend} />
+        <Card title="Mobile" technologies={mobile} />
+        <Card title="DevOps" technologies={devops} />
+        <Card title="Development" technologies={development} />
+        <Card title="AWS" technologies={AWS} />
+        <Card title="Testing" technologies={testing} />
+      </div>
     </section>
   );
 };
